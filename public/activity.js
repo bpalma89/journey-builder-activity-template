ef1e4112d02ff5a21f2ca4e65e6251a079349ef2
Bruno Palma <b.palma@accenture.com>
2022-06-08 15:35:19 +0100

    journey builder test changes

---
+++ public/activity.js
@@ -0,0 +1,16 @@
+var connection = new Postmonger.Session();
+
+connection.trigger("ready");
+
+connection.on("initActivity", function (data) {
+  //evento disparado quando se carrega no botão "Done" no Journey Builder
+  console.log("Activity was initiated.");
+});
+
+connection.on("clickedNext", function (data) {
+  //evento disparado quando se carrega no botão "Done" no Journey Builder
+  console.log("Next was clicked.");
+  var firstName = document.getElementById("fname").value();
+  var lastName = document.getElementById("lname").value();
+  console.log(firstName + " " + lastName);
+});
