25ceccd5146e4e207f18897d50ce19effd1e9f3c
Bruno Palma <b.palma@accenture.com>
2022-06-08 16:23:32 +0100

    new changes

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,105 +1,22 @@
-define(["js/postmonger"], function (Postmonger) {
+define(["postmonger"], function (Postmonger) {
   "use strict";
+  // console.log(postmonger);
+  console.log(Postmonger);
 
   var connection = new Postmonger.Session();
-  var authTokens = {};
-  var payload = {};
-  $(window).ready(onRender);
 
-  connection.on("initActivity", initialize);
-  connection.on("requestedTokens", onGetTokens);
-  connection.on("requestedEndpoints", onGetEndpoints);
-  connection.on("requestedInteraction", onRequestedInteraction);
-  connection.on(
-    "requestedTriggerEventDefinition",
-    onRequestedTriggerEventDefinition
-  );
-  connection.on("requestedDataSources", onRequestedDataSources);
+  connection.trigger("ready");
 
-  connection.on("clickedNext", save);
+  connection.on("initActivity", function (data) {
+    //evento disparado quando se carrega no botão "Done" no Journey Builder
+    console.log("Activity was initiated.");
+  });
 
-  function onRender() {
-    // JB will respond the first time 'ready' is called with 'initActivity'
-    connection.trigger("ready");
-
-    connection.trigger("requestTokens");
-    connection.trigger("requestEndpoints");
-    connection.trigger("requestInteraction");
-    connection.trigger("requestTriggerEventDefinition");
-    connection.trigger("requestDataSources");
-  }
-
-  function onRequestedDataSources(dataSources) {
-    console.log("*** requestedDataSources ***");
-    console.log(dataSources);
-  }
-
-  function onRequestedInteraction(interaction) {
-    console.log("*** requestedInteraction ***");
-    console.log(interaction);
-  }
-
-  function onRequestedTriggerEventDefinition(eventDefinitionModel) {
-    console.log("*** requestedTriggerEventDefinition ***");
-    console.log(eventDefinitionModel);
-  }
-
-  function initialize(data) {
-    console.log(data);
-    if (data) {
-      payload = data;
-    }
-
-    var hasInArguments = Boolean(
-      payload["arguments"] &&
-        payload["arguments"].execute &&
-        payload["arguments"].execute.inArguments &&
-        payload["arguments"].execute.inArguments.length > 0
-    );
-
-    var inArguments = hasInArguments
-      ? payload["arguments"].execute.inArguments
-      : {};
-
-    console.log(inArguments);
-
-    $.each(inArguments, function (index, inArgument) {
-      $.each(inArgument, function (key, val) {});
-    });
-
-    connection.trigger("updateButton", {
-      button: "next",
-      text: "done",
-      visible: true,
-    });
-  }
-
-  function onGetTokens(tokens) {
-    console.log(tokens);
-    authTokens = tokens;
-  }
-
-  function onGetEndpoints(endpoints) {
-    console.log(endpoints);
-  }
-
-  function save() {
-    /*var postcardURLValue = $('#postcard-url').val();
-        var postcardTextValue = $('#postcard-text').val();
-
-        payload['arguments'].execute.inArguments = [{
-            "tokens": authTokens
-        }];
-        
-        payload['metaData'].isConfigured = true;
-
-        console.log(payload);
-        connection.trigger('updateActivity', payload);*/
-
+  connection.on("clickedNext", function (data) {
     //evento disparado quando se carrega no botão "Done" no Journey Builder
     console.log("Next was clicked.");
     var firstName = document.getElementById("fname").value();
     var lastName = document.getElementById("lname").value();
     console.log(firstName + " " + lastName);
-  }
+  });
 });
