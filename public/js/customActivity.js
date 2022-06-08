7fdccf3fd82f42734c57ea06d3ae7542641c4d7c
Bruno Palma <b.palma@accenture.com>
2022-06-08 16:09:13 +0100

    new changes

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,4 +1,4 @@
-define(["postmonger"], function (Postmonger) {
+/*define(["postmonger"], function (Postmonger) {
   "use strict";
 
   var connection = new Postmonger.Session();
@@ -106,4 +106,21 @@ define(["postmonger"], function (Postmonger) {
     var lastName = document.getElementById("lname").value();
     console.log(firstName + " " + lastName);
   }
+});*/
+
+var connection = new Postmonger.Session();
+
+connection.trigger("ready");
+
+connection.on("initActivity", function (data) {
+  //evento disparado quando se carrega no botão "Done" no Journey Builder
+  console.log("Activity was initiated.");
+});
+
+connection.on("clickedNext", function (data) {
+  //evento disparado quando se carrega no botão "Done" no Journey Builder
+  console.log("Next was clicked.");
+  var firstName = document.getElementById("fname").value();
+  var lastName = document.getElementById("lname").value();
+  console.log(firstName + " " + lastName);
 });
